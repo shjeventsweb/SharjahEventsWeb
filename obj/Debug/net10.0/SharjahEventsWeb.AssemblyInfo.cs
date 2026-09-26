58d9e917f0fb9daf818ac91f7eb53cee9fb68b6f
@ionedge <ionedge@zohomail.com>
2026-09-26 18:55:47 +0300

    Final fix for HomeController structure

--- obj/Debug/net10.0/SharjahEventsWeb.AssemblyInfo.cs
+++ obj/Debug/net10.0/SharjahEventsWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharjahEventsWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f208aadc6759faab7b3f5a7fd021bd4752b63fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+361c561c69ecaff53318cb26a6b59e6a97427337")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharjahEventsWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharjahEventsWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
